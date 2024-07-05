1a366e736400c728a33db382d757f38f5da8d7c3
darshil929 <darshilt929@gmail.com>
2024-07-05 16:10:04 +0530

    started with gan implementation

---
+++ app.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+from torch.utils.tensorboard import SummaryWriter
+
+class Discriminator(nn.Module):
+    def __init__(self, in_features):
+        super().__init__()
+        self.disc = nn.Sequential(
+            nn.Linear(in_features, 128),
+            nn.LeakyReLU(0.1),
+            nn.Linear(128, 1),
+            nn.Sigmoid(),
+        )
