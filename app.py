c65af64c5227f4693ca16a51fa5d5dfa66e94ee8
darshil929 <darshilt929@gmail.com>
2024-07-05 16:19:05 +0530

    classes discriminator and generator

--- app.py
+++ app.py
@@ -16,3 +16,19 @@ class Discriminator(nn.Module):
             nn.Linear(128, 1),
             nn.Sigmoid(),
         )
+        
+    def forward(self, x):
+        return self.disc(x)
+    
+class Generator(nn.Module):
+    def __init__(self, z_dim, img_dim):
+        super().__init__()
+        self.gen = nn.Sequential(
+            nn.Linear(z_dim, 256),
+            nn.LeakyReLU(0.1),
+            nn.Linear(256, img_dim), # 28x28 --> 784
+            nn.Tanh(),
+        )
+        
+    def forward(self, x):
+        return self.gen(x)
